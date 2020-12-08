3a0341b4645e719003fdb52377100b2de1acefcd
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2020-12-08 13:41:03 +0530

    update:: css& html

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>GoodMovies</h1>
+      <h3>Checkout My favourite Movies. Select a Genre to get started</h3>
+      <button>Action</button>
+      <button>Comedy</button>
+      <button>Horror</button>
+      <button>Romance</button>
+      <button>Sci-Fic</button>
+      <button>Biopic</button>
+      <button>Thriller</button>
     </div>
   );
 }
